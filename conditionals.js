d9cf235424c49d2a8d1480d16db793d27d754343
Kim Chiu <chiu.kimberleyk@gmail.com>
2020-03-06 06:46:45 +0000

    practice conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,14 @@
-var raining = false;
-if (!raining) {
-  console.log("Leave your umbrella at home!");
+const whichSchool = function (age) {
+
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else if (age > 18) {
+    return "Lighthouse Labs";
+  }
 }
+
+age = 15;
+console.log("I am " + age + " . Which school should I go to?");
+console.log(whichSchool(age));
