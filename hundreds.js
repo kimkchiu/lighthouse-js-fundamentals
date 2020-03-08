c6e1c4d271e8c6b4e4f947d61e6d805fe681c03d
Kim Chiu <chiu.kimberleyk@gmail.com>
2020-03-08 13:57:58 +0000

    added more new files

---
+++ hundreds.js
@@ -0,0 +1,19 @@
+function howManyHundreds(num) {
+
+  let output;
+
+  if (num % 100 === 0) {
+    output = num / 100;
+
+  } else {
+    output = Math.floor(num / 100);
+
+  }
+  return output;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
